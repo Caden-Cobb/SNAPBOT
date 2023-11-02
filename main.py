f408ec434cad58312f80b198fb05ec9f73e3357e
Caden Cobb <cobbb.caden@Cobbs-MBP.attlocal.net>
2023-11-01 23:54:15 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+import schedule
+
+def refresh_page():
+    pyautogui.press('q')
+    pyautogui.hotkey('command', 'r')
+    time.sleep(60)
+    pyautogui.press('q')
+
+def main():
+    time.sleep(5)  # Wait for 5 seconds
+    refresh_page()
+    schedule.every().hour.do(refresh_page)  # Back to running every hour
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
